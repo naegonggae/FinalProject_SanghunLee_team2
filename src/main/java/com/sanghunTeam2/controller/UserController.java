15705fb26f9d83a80057e46c638a5c4ae9d809e6
naegonggae <https://github.com/naegonggae/>
2022-12-20 11:37:32 +0900

    Dockerfile 푸시, api/v1/users ;hello 출력문 작성

---
+++ src/main/java/com/sanghunTeam2/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.sanghunTeam2.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok().body("Hello");
+    }
+}
