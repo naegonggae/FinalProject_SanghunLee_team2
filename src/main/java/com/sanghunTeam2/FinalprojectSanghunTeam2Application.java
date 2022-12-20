488e5fd66f6a73b0adbaa60d5b4c6f32b76bec3d
naegonggae <https://github.com/naegonggae/>
2022-12-20 11:32:22 +0900

    project build

---
+++ src/main/java/com/sanghunTeam2/FinalprojectSanghunTeam2Application.java
@@ -0,0 +1,13 @@
+package com.sanghunTeam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalprojectSanghunTeam2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalprojectSanghunTeam2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanghunTeam2/FinalprojectSanghunTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanghunTeam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectSanghunTeam2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
